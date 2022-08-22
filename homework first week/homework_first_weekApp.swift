15a902212dc86ae2b790aa6cd48769834958dd2a
Chuck Japely <chuckjapely@gmail.com>
2022-08-22 08:30:04 -0700

    Initial Commit

---
+++ homework first week/homework_first_weekApp.swift
@@ -0,0 +1,17 @@
+//
+//  homework_first_weekApp.swift
+//  homework first week
+//
+//  Created by Chuck Japely on 8/22/22.
+//
+
+import SwiftUI
+
+@main
+struct homework_first_weekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
